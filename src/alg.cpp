d071cbe638c072a38b45f9575838032d550abec0
ivanzakharov78902543 <125127109+ivanzakharov78902543@users.noreply.github.com>
2023-03-10 21:55:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -19,29 +19,29 @@ uint64_t fact(uint16_t n) {
 }
 
 double calcItem(double x, uint16_t n) {
-    return double(pown(x, n) / fact(n));
+    return <double>pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
     double ex = 1;
     for (uint16_t i = 1; i <= count; i++) {
-        ex += double(pown(x, i) / fact(i));
+        ex += <double>pown(x, i) / fact(i);
     }
     return ex;
 }
 
 double sinn(double x, uint16_t count) {
-    double sin = x;
-    for (uint16_t i = 2; i <= count; i++) {
-        sin += pown(-1, i - 1) * (double(pown(x, 2 * i - 1) / fact(2 * i - 1)));
+    double sin = 0;
+    for (uint16_t i = 1; i <= count; i++) {
+        sin += pown(-1, i - 1) * (<double>pown(x, 2 * i - 1) / fact(2 * i - 1));
     }
     return sin;
 }
 
 double cosn(double x, uint16_t count) {
-    double cos = 1;
-    for (uint16_t i = 2; i <= count; i++) {
-        cos += pown(-1, i - 1) * (double(pown(x, 2 * i - 2) / fact(2 * i - 2)));
+    double cos = 0;
+    for (uint16_t i = 1; i <= count; i++) {
+        cos += pown(-1, i - 1) * (<double>pown(x, 2 * i - 2) / fact(2 * i - 2));
     }
     return cos;
 }
