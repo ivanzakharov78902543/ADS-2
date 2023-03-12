c1c43c6035745daa43e791278e4892c8c5ede01a
ivanzakharov78902543 <125127109+ivanzakharov78902543@users.noreply.github.com>
2023-03-13 02:06:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -35,16 +35,16 @@ double expn(double x, uint16_t count) {
 
 double sinn(double x, uint16_t count) {
     double sin = 0;
-    for (uint16_t i = 1; i <= count + 1; i++) {
-        sin += pown(-1, i - 1) * (pown(x, 2 * i - 1) / fact(2 * i - 1));
+    for (uint16_t i = 1; i <= count; i++) {
+        sin += pown(-1, i - 1) * calcItem(x, 2 * i - 1);
     }
     return sin;
 }
 
 double cosn(double x, uint16_t count) {
     double cos = 0;
-    for (uint16_t i = 1; i <= count + 1; i++) {
-        cos += pown(-1, i - 1) * (pown(x, 2 * i - 2) / fact(2 * i - 2));
+    for (uint16_t i = 1; i <= count; i++) {
+        cos += pown(-1, i - 1) * calcItem(x, 2 * i - 2));
     }
     return cos;
 }
